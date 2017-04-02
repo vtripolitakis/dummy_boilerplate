e8a136830d141f9f9aa46fce9b6d23696ace0127
vtripolitakis <vtripolitakis@gmail.com>
2017-04-02 21:39:53 +0300

    [CHG] updated package and demo source to pass linter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+/*eslint-disable no-unused-vars*/
 import React from "react"
+/*eslint-enable no-unused-vars*/
 import ReactDOM from "react-dom"
+
+/*eslint-disable no-unused-vars*/
 import Hello from "./components/hello.js"
 import World from "./components/world"
-
+/*eslint-enable no-unused-vars*/
 ReactDOM.render(<div><Hello/><World/></div>, document.getElementById("root"))

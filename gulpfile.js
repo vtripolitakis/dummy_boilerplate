a8ad52c830723ee6f9a4f2b111bff0fcb9518c4c
vaggelis <vaggelis@Evangeloss-MacBook-Pro.local>
2017-04-02 19:08:20 +0300

    [ADD] added compress task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
+var minify = require("gulp-minify");
 
 gulp.task("default", function () {
   return gulp.src("src/**/*.js")
@@ -12,4 +13,17 @@ gulp.task("default", function () {
     .pipe(concat("all.js"))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
+});
+
+gulp.task('compress', function() {
+  gulp.src('dist/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
 });

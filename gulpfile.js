93eb70042891b725cf01db63e9fd6c2b7f10b10e
vaggelis <vaggelis@Evangeloss-MacBook-Pro.local>
2017-04-02 19:28:18 +0300

    [ADD] ESLint support

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 var minify = require("gulp-minify");
+const eslint = require('gulp-eslint');
+
 
 gulp.task("default", function () {
   return gulp.src("src/**/*.js")
@@ -16,7 +18,7 @@ gulp.task("default", function () {
 });
 
 gulp.task('compress', function() {
-  gulp.src('dist/*.js')
+  gulp.src('dist/**/*.js')
     .pipe(minify({
         ext:{
             src:'-debug.js',
@@ -27,3 +29,21 @@ gulp.task('compress', function() {
     }))
     .pipe(gulp.dest('dist'))
 });
+
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['src/**','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
